2caad9c35daac976e8309beb9a62c18beeaea4fb
Diyorbek Ismoilov <85097020+diyorbekismoilov@users.noreply.github.com>
2022-04-17 05:36:07 +0500

    edited 17.04.2022

--- random.cpp
+++ random.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+#include <stdio.h>     
+#include <stdlib.h>     
+#include <time.h>      
+using namespace std;
+int main ()
+{
+  int iSecret, iGuess;
+  srand (time(NULL));
+  iSecret = rand() % 100 + 1;  /* random son */
+  do {
+    cout << "Tahminingizni kiriting: ";  /* input */
+	cin >> iGuess;
+    if (iSecret<iGuess) puts ("Noma'lum son kichikroq!");
+    else if (iSecret>iGuess) puts ("Noma'lum son kattaroq!");
+  } while (iSecret!=iGuess);
+  printf("To'g'ri bu son %d edi!\n",iSecret);
+  return 0;
+}
